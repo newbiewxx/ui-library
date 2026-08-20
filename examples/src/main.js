67ff2c6b6347a322d4e77dee192115c362a58e75
jetwang88 <649125397@qq.com>
2026-08-20 22:10:37 +0800

    feat: 初始化 theme 主题包

--- examples/src/main.js
+++ examples/src/main.js
@@ -3,6 +3,12 @@ import "./style.css";
 import App from "./App.vue";
 import AUi from "../../packages";
 
+// 全量导入主题包
+import "@ui-library/theme/src/index.scss";
+
+// 按需导入
+// import "@ui-library/theme/src/button.scss";
+
 const app = createApp(App);
 app.use(AUi);
 app.mount("#app");
