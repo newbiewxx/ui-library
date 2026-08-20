318f3edc7f635da1179b1a5f07eeb0d6b2aa1019
jetwang88 <649125397@qq.com>
2026-08-20 22:14:29 +0800

    feat: 初始化 theme 主题包

--- examples/src/main.js
+++ examples/src/main.js
@@ -8,6 +8,8 @@ import "@ui-library/theme/src/index.scss";
 
 // 按需导入
 // import "@ui-library/theme/src/button.scss";
+// 按需导入主题包
+// import "@ui-library/components/button/src/style";
 
 const app = createApp(App);
 app.use(AUi);
