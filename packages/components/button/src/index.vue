3733071f4ca421440627d3362777e695a87f4a51
jetwang88 <649125397@qq.com>
2026-08-20 21:47:31 +0800

    feat: 封装 useNamespace hook 去自动生成 BEM 类名

--- packages/components/button/src/index.vue
+++ packages/components/button/src/index.vue
@@ -4,13 +4,16 @@ defineOptions({
   name: "AButton",
 });
 
-const ns = useNamespace();
+const ns = useNamespace("button");
 
 console.log("组件的命名空间: => ", ns.namespace);
+console.log("组件的块类名: => ", ns.b("wrapper"));
+console.log(ns.e("item"));
+console.log(ns.m("rounded"))
 </script>
 
 <template>
-  <button>测试按钮</button>
+  <button :class="[ns.b(), ns.m('primary')]">测试按钮</button>
 </template>
 
 <style></style>
--- packages/hooks/use-namespace/index.js
+++ packages/hooks/use-namespace/index.js
@@ -1,9 +1,39 @@
 const defaultNamespace = "a";
 
-export const useNamespace = () => {
+// el-button
+// suffix => el-button-wrapper
+const _bem = (namespace, block, blockSuffix, element, modifier, modifierValue) => {
+  let className = `${namespace}-${block}`;
+
+  blockSuffix && (className += `-${blockSuffix}`);
+  element && (className += `__${element}`);
+  modifier && (className += `--${modifier}`);
+  modifierValue && (className += `--${modifierValue}`);
+  return className;
+};
+
+export const useNamespace = block => {
   const namespace = defaultNamespace;
+
+  const b = (blockSuffix = "") => _bem(namespace, block, blockSuffix);
+  const e = (element = "") => (element ? _bem(namespace, block, "", element) : "");
+  const m = (modifier = "") => _bem(namespace, block, "", "", modifier);
+
+  const be = (blockSuffix = "", element = "") =>
+    blockSuffix && element ? _bem(namespace, block, blockSuffix, element) : "";
+
+  const em = (blockSuffix = "", modifier = "") =>
+    blockSuffix && modifier ? _bem(namespace, block, blockSuffix, "", modifier) : "";
+
+  const is = (activeName, state) => (activeName && state ? `is-${activeName}`: '');
 
   return {
     namespace,
+    b,
+    e,
+    m,
+    be,
+    em,
+    is
   };
 };
