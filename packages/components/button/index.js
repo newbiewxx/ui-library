1f3ea7040749ba34cf8761c81ed98bf0dc7f0758
jetwang88 <649125397@qq.com>
2026-08-20 19:34:49 +0800

    feat: export components

--- packages/components/button/index.js
+++ packages/components/button/index.js
@@ -1,1 +1,7 @@
 // 每个组件的文件下均包含一个 index.js，用于导出组件
+import Button from "./src/index.vue";
+import { componentInstall } from "@ui-library/utils";
+
+export default componentInstall(Button);
+export { Button };
+
